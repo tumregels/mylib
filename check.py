26a1cfd68ca00707d7bc298c40e5fa0ab54988fe
tumregels <4774418+tumregels@users.noreply.github.com>
2023-07-25 17:52:13 -0400

    minor cleanup

--- check.py
+++ check.py
@@ -1,11 +1,12 @@
 import ctypes
 import pathlib
 
+
 if __name__ == "__main__":
     # Load the shared library into ctypes
     libname = pathlib.Path(__file__).parent.absolute() / "build" / "libconvert.so"
-    c_lib = ctypes.CDLL(libname)
+    clib = ctypes.CDLL(libname)
     celsius = 0.0
-    c_lib.convert_celsius_to_fahrenheit.restype = ctypes.c_float
-    answer = c_lib.convert_celsius_to_fahrenheit(ctypes.c_float(celsius))
+    clib.convert_celsius_to_fahrenheit.restype = ctypes.c_float
+    answer = clib.convert_celsius_to_fahrenheit(ctypes.c_float(celsius))
     print(answer)
